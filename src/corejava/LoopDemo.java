00c3eb40352fb32e3a29b9a8ada6fe64c272dac6
radhekrushn <118706055+radhekrushn@users.noreply.github.com>
2023-06-26 12:16:53 +0530

    26-6-23

--- src/corejava/LoopDemo.java
+++ src/corejava/LoopDemo.java
@@ -1,10 +1,50 @@
 package corejava;
 
+import java.util.Scanner;
+
 public class LoopDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	/*	int i=1;
+		
+		while(i<=10)
+		{
+			System.out.println(i+". Hinal");
+			i++;
+		}
+		
+   
+		int i=1;
+		
+		do
+		{
+			System.out.println(i+". Hinal");
+			i++;
+		}
+		while(i<=100);
+		
+	
+		
+		// for Loop demo
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+		*/
+		
+		// WAJP ti accept a no.
+		
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number.");
+		num = sc.nextInt();
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(num+" X "+i+" = "+(num*i));
+		}
 	}
 
 }
