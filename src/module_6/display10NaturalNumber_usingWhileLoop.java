dfda94dc054683c835a006a24d3627e0c4aa504a
radhekrushn <118706055+radhekrushn@users.noreply.github.com>
2023-06-29 12:36:04 +0530

    29-6-23

---
+++ src/module_6/display10NaturalNumber_usingWhileLoop.java
@@ -0,0 +1,23 @@
+package module_6;
+
+import java.util.Scanner;
+
+public class display10NaturalNumber_usingWhileLoop {
+
+	public static void main(String[] args) {
+		
+		int A=1;
+		
+		while(A<=10)
+		{
+			System.out.println(A+"");
+			A++;
+		}
+		
+		
+		
+		
+
+	}
+
+}
