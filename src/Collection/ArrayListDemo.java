73616e87264b7f2466527af46aee08f5e8f938fe
radhekrushn <118706055+radhekrushn@users.noreply.github.com>
2023-07-15 12:42:26 +0530

    15-7-23

---
+++ src/Collection/ArrayListDemo.java
@@ -0,0 +1,28 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		     ArrayList list = new ArrayList();
+		     
+		     list.add(1);
+		     list.add(6.5f);
+		     list.add(true);
+		     list.add('H');
+		     list.add(null);
+		     list.add(false);
+		     list.add(0);
+		     list.add(12);
+		     list.add(1);
+		     list.add(true);
+		     list.add("Hinal");
+		     list.add("I am learning automation testing.");
+		     System.out.println(list);
+		
+
+	}
+
+}
---
+++ src/Collection/HashMapDemo.java
@@ -0,0 +1,14 @@
+package Collection;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		HashMap hm = new HashMap();
+		
+	}
+
+}
